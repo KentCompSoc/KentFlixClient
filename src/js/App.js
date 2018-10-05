9d4d1f035ff9ad3b5e388907ffbf3ba88cee858f
HenryBrown0 <henrybrown444@gmail.com>
2018-10-05 16:32:40 +0100

    Add mini css

---
+++ src/js/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Some text</div>
+    );
+  }
+}
+
+export default App;
