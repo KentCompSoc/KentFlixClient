f11bb75d49c6918a3b260494de070e3515b7eda6
HenryBrown0 <henrybrown444@gmail.com>
2018-10-08 11:45:46 +0100

    Add theme change

--- src/js/App.js
+++ src/js/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import Home from './routes/Home';
 import Profile from './routes/Profile';
 import Login from './routes/Login';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
@@ -26,35 +27,7 @@ class App extends Component {
             <Route exact path="/login/" component={Login} />
             <Route exact path="/profile/" component={Profile} />
           </div>
-
-          <footer>
-            <p>
-              Made with <i className="far fa-heart" title="love" aria-hidden>
-              </i> <span className="sr-only">love</span> by students for
-              students
-            </p>
-            <p><Link to="/about/">About</Link></p>
-            <div className="button small">
-              <a href="https://github.com/KentCompSoc/KentFlixClient/">
-                <i
-                  className="fab fa-github"
-                  title="GitHub link"
-                  aria-hidden
-                ></i>
-                <span className="sr-only">GitHub link</span>
-              </a>
-            </div>
-            <div className="button small">
-              <a href="https://www.facebook.com/groups/kentcomputingsociety/">
-                <i
-                  className="fab fa-facebook-square"
-                  title="Facebook link"
-                  aria-hidden
-                ></i>
-                <span className="sr-only">Facebook link</span>
-              </a>
-            </div>
-          </footer>
+          <Footer />
         </div>
       </Router>
     );
