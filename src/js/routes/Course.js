57f619054f6117cc4c7f2e7d550f8d289b85ce2c
HenryBrown0 <henrybrown444@gmail.com>
2018-10-09 17:39:17 +0100

    Sort videos by date. Newest first

--- src/js/routes/Course.js
+++ src/js/routes/Course.js
@@ -7,31 +7,40 @@ import videos from "../testVideos.json"
 const Course = (props) => (
   <div className="row">
     {console.log(videos)}
-    <div className="col-sm-12">{props.match.params.uri}</div>
-    {videos.video.map(v => (
-      <div className="col-sm-12 col-md-4 col-lg-3" key={v.publish}>
-        <div className="card fluid">
-          <img
-            src="image.png"
-            className="section media"
-            alt={"Watch " + v.title + " video"}
-          />
-          <progress
-            className="video-progress"
-            value={v.progress}
-            max={v.duration}
-          ></progress>
-          <h4>
-            {v.title} - {v.author}
-            <small>{v.publish}</small>
-          </h4>
-          <div className="button-group">
-            <button className="video-btn-1">View</button>
-            <button className="video-btn-1">Watch later</button>
+    <div className="col-sm-12">
+      <img src={videos.image} alt={videos.title + " course image"} />
+    </div>
+    <div className="col-sm-12">
+      <h3>
+        {videos.title}
+        <small>{videos.video.length} videos available to watch</small>
+      </h3>
+    </div>
+    {videos.video.sort((x, y) => new Date(y.publish) - new Date(x.publish))
+      .map(v => (
+        <div className="col-sm-12 col-md-4 col-lg-3" key={v.publish}>
+          <div className="card fluid">
+            <img
+              src="image.png"
+              className="section media"
+              alt={"Watch " + v.title + " video"}
+            />
+            <progress
+              className="video-progress"
+              value={v.progress}
+              max={v.duration}
+            ></progress>
+            <h4>
+              {v.title} - {v.author}
+              <small>{new Date(v.publish).toUTCString()}</small>
+            </h4>
+            <div className="button-group">
+              <button className="video-btn-1">View</button>
+              <button className="video-btn-1">Watch later</button>
+            </div>
           </div>
         </div>
-      </div>
-    ))}
+      ))}
 
   </div>
 );
