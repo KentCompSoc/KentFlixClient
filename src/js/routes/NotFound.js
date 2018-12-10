7658b595a936ad5359513b3009dcc058858a98ba
HenryBrown0 <spike.1@live.co.uk>
2018-12-10 08:50:05 +0000

    Update not found to material ui

--- src/js/routes/NotFound.js
+++ src/js/routes/NotFound.js
@@ -1,18 +1,35 @@
+// React
 import React from 'react';
+// Material-UI
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+// Styles
+const styles = {
+	root: {
+		padding: 5,
+	},
+};
+const NotFound = props => {
+	const { classes } = props;
+	return (
+		<div className={classes.root}>
+			<Grid container spacing={8}>
+				<Grid item xs={12}>
+					<Typography variant="h2" gutterBottom>
+						Couldn't find that page
+					</Typography>
+					<Button
+						variant="outlined"
+						href="https://github.com/KentCompSoc/KentFlixClient/issues/new?template=bug_report.md"
+					>
+						Report a bug
+					</Button>
+				</Grid>
+			</Grid>
+		</div>
+	)
+}
 
-const NotFound = () => (
-	<div className="row">
-		<h3>
-			Couldn't find that page
-			<small>
-				<a
-					href="https://github.com/KentCompSoc/KentFlixClient/issues/new?template=bug_report.md"
-				>
-					Report a bug
-				</a>
-			</small>
-		</h3>
-	</div>
-)
-
-export default NotFound;
+export default withStyles(styles)(NotFound)
